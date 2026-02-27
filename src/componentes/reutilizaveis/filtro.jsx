a39f569a5b068ee1fc1e981db6d3569355f0c382
EduardoMoyses <153742466+EduardoMoyses@users.noreply.github.com>
2026-02-27 11:24:32 -0300

    atualização do filtro

--- src/componentes/reutilizaveis/filtro.jsx
+++ src/componentes/reutilizaveis/filtro.jsx
@@ -2,28 +2,32 @@ import Botao from "./botao"
 import Styles from './filtro.module.css'
 import receitas from '../../data/receitas'
 
-
-function Filtro({ativo, onClose, onSelectCategoria}) {
-    return (
-    <div className={`${Styles.filtroContainer} ${ativo ? Styles.ativo : ''}`}>
+function Filtro({ ativo, onClose, onSelectCategoria }) {
+  return (
+    <div className={`${Styles.overlay} ${ativo ? Styles.ativo : ''}`}>
+      
+      <div className={Styles.filtroContainer}>
         <div className={Styles.botaoFiltro}>
-            <Botao name={<i className="fa-solid fa-x"></i>} onClick={onClose} />
+          <Botao name={<i className="fa-solid fa-x"></i>} onClick={onClose} />
         </div>
-        <div className={Styles.filtroInfo}>
-            <h2>Escolha uma Categoria</h2>
-            <ul>
-                {receitas.categorias.map(categoria => (
-                <li
-                    key={categoria.id}
-                    onClick={() => onSelectCategoria(categoria.id)}>
-                {categoria.nome}
-                </li>
-                ))}
-            </ul>
 
+        <div className={Styles.filtroInfo}>
+          <h2>Escolha uma Categoria</h2>
+          <ul>
+            {receitas.categorias.map(categoria => (
+              <li
+                key={categoria.id}
+                onClick={() => onSelectCategoria(categoria.id)}
+              >
+                {categoria.nome}
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
+
     </div>
-    )
+  )
 }
 
 export default Filtro
